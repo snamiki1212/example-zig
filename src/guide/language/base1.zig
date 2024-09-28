09f1239a93c9bed1477d56e87e48bfb56f42c30d
snamiki1212 <snamiki1212@gmail.com>
2024-09-28 12:42:35 +0900

    fix: update

--- src/guide/language/base1.zig
+++ src/guide/language/base1.zig
@@ -1,3 +1,5 @@
+const expect = @import("std").testing.expect;
+
 pub fn main() !void {
     { // assignemnt
         const constant: i32 = 5;
@@ -10,5 +12,31 @@ pub fn main() !void {
         const a = [5]u8{ 'h', 'e', 'l', 'l', 'o' };
         const l = a.len;
         _ = l; // autofix
+    }
+}
+
+test "if" {
+    // if statement
+    if (true) {
+        try expect(true);
+    } else {
+        try expect(false);
+    }
+
+    { // one liner
+        const x = if (true) 1 else "1";
+        try expect(x == 1);
+    }
+}
+
+test "for" {
+    const str = [_]u8{ 'h', 'e', 'l', 'l', 'o' };
+    for (str) |c| {
+        try expect(c != 'x');
+    }
+
+    for (str, 0..) |c, i| {
+        try expect(c != 'x');
+        try expect(i < str.len);
     }
 }
